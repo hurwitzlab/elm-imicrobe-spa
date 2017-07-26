c17b0045337c970692cb3a351a01cb209b57e6e6
Ken Youens-Clark <kyclark@gmail.com>
2017-07-26 16:45:29 -0700

    mucho work on metasearch, but it basically does everything I wanted -- huzzah!

--- src/Page/MetaSearch.elm
+++ src/Page/MetaSearch.elm
@@ -2,12 +2,15 @@ module Page.MetaSearch exposing (Model, Msg, init, update, view)
 
 import Data.MetaSearch
 import Dict
+import Exts.Dict as EDict
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onCheck, onClick, onInput, onSubmit)
 import Http
 import Json.Decode as Decode
 import Json.Encode as Encode
+import List.Extra
+import MultiSelect as Multi
 import Page.Error as Error exposing (PageLoadError, pageLoadError)
 import RemoteData exposing (..)
 import Request.MetaSearch
@@ -34,8 +37,10 @@ type alias Model =
     , query : String
     , params : Dict.Dict String String
     , selectedParams : List ( String, String )
-    , optionValues : Dict.Dict String String
+    , optionValues : Dict.Dict String (List String)
     , searchResults : WebData (List (Dict.Dict String JsonType))
+    , possibleOptionValues : Dict.Dict String (List JsonType)
+    , restrictedParams : Dict.Dict String String
     }
 
 
@@ -72,6 +77,8 @@ init =
                 , selectedParams = []
                 , optionValues = Dict.empty
                 , searchResults = NotAsked
+                , possibleOptionValues = Dict.empty
+                , restrictedParams = Dict.empty
                 }
         )
         loadParams
@@ -86,6 +93,8 @@ type Msg
     = AddParamOption String
     | RemoveOption String
     | UpdateOptionValue String String
+    | UpdateMultiOptionValue String (List String)
+    | UpdatePossibleOptionValues (Result Http.Error (Dict.Dict String (List JsonType)))
     | Search
     | UpdateSearchResults (WebData (List (Dict.Dict String JsonType)))
 
@@ -94,26 +103,34 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         AddParamOption opt ->
-            ( { model | selectedParams = addSelectedParam model opt }, Cmd.none )
+            ( { model | selectedParams = addSelectedParam model opt }
+            , getParamValues opt model.optionValues model.params
+            )
 
         RemoveOption opt ->
             let
-                newOptionValues =
-                    rmOptionValue model.optionValues opt
+                newModel =
+                    { model
+                        | selectedParams = rmParam model.selectedParams opt
+                        , optionValues =
+                            rmOptionValue model.optionValues opt
+                    }
             in
-            ( { model
-                | selectedParams = rmParam model.selectedParams opt
-                , optionValues = rmOptionValue model.optionValues opt
-              }
-            , doSearch newOptionValues
-            )
+            ( newModel, doSearch newModel )
 
         Search ->
-            ( model, doSearch model.optionValues )
+            ( model, doSearch model )
 
         UpdateOptionValue opt val ->
             ( { model
-                | optionValues = Dict.insert opt val model.optionValues
+                | optionValues = Dict.insert opt [ val ] model.optionValues
+              }
+            , Cmd.none
+            )
+
+        UpdateMultiOptionValue opt vals ->
+            ( { model
+                | optionValues = Dict.insert opt vals model.optionValues
               }
             , Cmd.none
             )
@@ -121,9 +138,20 @@ update msg model =
         UpdateSearchResults response ->
             ( { model
                 | searchResults = response
+                , restrictedParams = mkRestrictedParams model.params response
 
-                -- , restrictedOptionList = mkRestrictedOptionList model.optionList response
                 -- , restrictedOptionValues = mkRestrictedOptionValues response
+              }
+            , Cmd.none
+            )
+
+        UpdatePossibleOptionValues (Err err) ->
+            ( model, Cmd.none )
+
+        UpdatePossibleOptionValues (Ok response) ->
+            ( { model
+                | possibleOptionValues =
+                    Dict.union response model.possibleOptionValues
               }
             , Cmd.none
             )
@@ -140,10 +168,16 @@ view model =
             [ div [ style [ ( "text-align", "center" ) ] ]
                 [ h2 [] [ text model.pageTitle ]
                 , div [] (mkParamsSelect model)
-                , div [] [ mkOptionTable model.selectedParams ]
+                , div [] [ mkOptionTable model ]
+                , div [] [ showResults model ]
                 ]
-            , div [] [ text (toString model.optionValues) ]
-            , div [] [ text (toString model.searchResults) ]
+
+            {--
+            , div [] [ text ("restrictedParams = " ++ toString model.restrictedParams) ]
+            , div [] [ text ("optionValues = " ++ toString model.optionValues) ]
+            , div [] [ text ("searchResults = " ++ toString model.searchResults) ]
+            , div [] [ text ("possibleOptionValues" ++ toString model.possibleOptionValues) ]
+            --}
             ]
         ]
 
@@ -169,11 +203,19 @@ mkParamsSelect model =
         first =
             Html.option [] [ text "-- Select --" ]
 
+        paramList =
+            case Dict.isEmpty model.restrictedParams of
+                True ->
+                    model.params
+
+                _ ->
+                    model.restrictedParams
+
         alreadySelected =
             List.map Tuple.first model.selectedParams |> Set.fromList
 
         showKeys =
-            Dict.keys model.params
+            Dict.keys paramList
                 |> List.filter (\v -> not (Set.member v alreadySelected))
 
         rest =
@@ -221,11 +263,14 @@ addSelectedParam model optionName =
             model.selectedParams
 
 
-mkOptionTable : List ( String, String ) -> Html Msg
-mkOptionTable options =
+mkOptionTable : Model -> Html Msg
+mkOptionTable model =
     let
+        options =
+            model.selectedParams
+
         rows =
-            List.map mkOptionRow options
+            List.map (mkOptionRow model.possibleOptionValues) options
 
         searchButtonRow =
             [ tr []
@@ -236,18 +281,75 @@ mkOptionTable options =
     in
     case rows of
         [] ->
-            text "No options"
+            text ""
 
         _ ->
             table [ style [ ( "width", "100%" ) ] ]
                 (rows ++ searchButtonRow)
 
 
-mkOptionRow : ( String, String ) -> Html Msg
-mkOptionRow ( optionName, dataType ) =
+unpackJsonType : JsonType -> String
+unpackJsonType v =
+    case v of
+        FloatType n ->
+            toString n
+
+        IntType n ->
+            toString n
+
+        StrType s ->
+            s
+
+        ValueType x ->
+            toString x
+
+
+mkOptionRow : Dict.Dict String (List JsonType) -> ( String, String ) -> Html Msg
+mkOptionRow possibleOptionValues ( optionName, dataType ) =
     let
         title =
             [ th [] [ text (prettyName optionName) ] ]
+
+        vals =
+            case Dict.get optionName possibleOptionValues of
+                Just v ->
+                    v
+
+                _ ->
+                    []
+
+        minVal =
+            case List.take 1 vals of
+                x :: [] ->
+                    unpackJsonType x
+
+                _ ->
+                    ""
+
+        maxVal =
+            case List.drop (List.length vals - 1) vals of
+                x :: [] ->
+                    unpackJsonType x
+
+                _ ->
+                    ""
+
+        strVals =
+            let
+                mkOption s =
+                    Html.option [ value s ] [ text s ]
+            in
+            case List.length vals > 1000 of
+                True ->
+                    input
+                        [ onInput (UpdateOptionValue optionName)
+                        , type_ "text"
+                        , placeholder dataType
+                        ]
+                        []
+
+                False ->
+                    mkMultiSelect optionName vals
 
         minName =
             "min__" ++ optionName
@@ -260,17 +362,16 @@ mkOptionRow ( optionName, dataType ) =
                 "number" ->
                     [ td [ onInput (UpdateOptionValue minName) ]
                         [ text "Min: "
-                        , input [ type_ "text", placeholder "min", name minName ] []
+                        , input [ type_ "text", placeholder minVal, name minName ] []
                         ]
                     , td [ onInput (UpdateOptionValue maxName) ]
                         [ text "Max: "
-                        , input [ type_ "text", placeholder "max", name maxName ] []
+                        , input [ type_ "text", placeholder maxVal, name maxName ] []
                         ]
                     ]
 
                 _ ->
-                    [ td [ onInput (UpdateOptionValue optionName) ]
-                        [ input [ type_ "text", placeholder dataType ] [] ]
+                    [ td [] [ strVals ]
                     , td [] []
                     ]
 
@@ -281,12 +382,29 @@ mkOptionRow ( optionName, dataType ) =
     tr [] (title ++ el ++ buttons)
 
 
+mkMultiSelect : String -> List JsonType -> Html Msg
+mkMultiSelect optionName vals =
+    let
+        strings =
+            List.map unpackJsonType vals
+
+        items =
+            List.map (\s -> { value = s, text = s, enabled = True }) strings
+    in
+    Multi.multiSelect
+        { onChange = UpdateMultiOptionValue optionName
+        , items = items
+        }
+        []
+        []
+
+
 rmParam : List ( String, String ) -> String -> List ( String, String )
 rmParam paramsList optToRemove =
     List.filter (\( k, v ) -> k /= optToRemove) paramsList
 
 
-rmOptionValue : Dict.Dict String String -> String -> Dict.Dict String String
+rmOptionValue : Dict.Dict String (List String) -> String -> Dict.Dict String (List String)
 rmOptionValue optionValues optToRemove =
     let
         names =
@@ -301,40 +419,209 @@ rmOptionValue optionValues optToRemove =
         |> Dict.fromList
 
 
-doSearch : Dict.Dict String String -> Cmd Msg
-doSearch options =
+oneOfJsonType : Decode.Decoder JsonType
+oneOfJsonType =
+    [ Decode.string
+        |> Decode.map StrType
+    , Decode.int
+        |> Decode.map IntType
+    , Decode.float
+        |> Decode.map FloatType
+    , Decode.value
+        |> Decode.map ValueType
+    ]
+        |> Decode.oneOf
+
+
+serializeForm :
+    Dict.Dict String (List String)
+    -> Dict.Dict String String
+    -> Encode.Value
+serializeForm optionValues paramTypes =
+    let
+        mkFloats =
+            List.filterMap (String.toFloat >> Result.toMaybe)
+
+        encodeVals param vals =
+            let
+                paramName =
+                    case
+                        String.startsWith "min__" param
+                            || String.startsWith "max__" param
+                    of
+                        True ->
+                            String.dropLeft 5 param
+
+                        False ->
+                            param
+
+                dataType =
+                    EDict.getWithDefault "string" paramName paramTypes
+
+                enc f xs =
+                    case xs of
+                        [] ->
+                            Encode.null
+
+                        x :: [] ->
+                            f x
+
+                        _ ->
+                            Encode.list (List.map f xs)
+            in
+            case dataType of
+                "number" ->
+                    enc Encode.float (mkFloats vals)
+
+                _ ->
+                    enc Encode.string vals
+    in
+    Dict.toList optionValues
+        |> List.map (\( k, vs ) -> ( k, encodeVals k vs ))
+        |> Encode.object
+
+
+doSearch : Model -> Cmd Msg
+doSearch model =
     let
         url =
-            -- "https://www.imicrobe.us/sample/search_results.json"
             "http://localhost:3006/samplesearch"
 
-        dictList =
-            Dict.toList options
-
-        encoded =
-            Encode.object
-                (List.map (\( k, v ) -> ( k, Encode.string v )) dictList)
-
         body =
-            Http.jsonBody encoded
+            serializeForm model.optionValues model.params
+                |> Http.jsonBody
 
         decoderDict =
-            [ Decode.string
-                |> Decode.map StrType
-            , Decode.int
-                |> Decode.map IntType
-            , Decode.float
-                |> Decode.map FloatType
-            , Decode.value
-                |> Decode.map ValueType
-            ]
-                |> Decode.oneOf
-                |> Decode.dict
+            Decode.dict oneOfJsonType
 
         decoder =
-            -- Decode.at [ "samples" ] (Decode.list decoderDict)
             Decode.list decoderDict
     in
     Http.post url body decoder
         |> RemoteData.sendRequest
         |> Cmd.map UpdateSearchResults
+
+
+getParamValues :
+    String
+    -> Dict.Dict String (List String)
+    -> Dict.Dict String String
+    -> Cmd Msg
+getParamValues optionName optionValues params =
+    let
+        url =
+            "http://localhost:3006/search_param_values"
+
+        decoder =
+            Decode.dict (Decode.list oneOfJsonType)
+
+        body =
+            Encode.object
+                [ ( "param", Encode.string optionName )
+                , ( "query", serializeForm optionValues params )
+                ]
+                |> Http.jsonBody
+    in
+    Http.post url body decoder
+        |> Http.send UpdatePossibleOptionValues
+
+
+showResults : Model -> Html msg
+showResults model =
+    case model.searchResults of
+        NotAsked ->
+            text ""
+
+        Loading ->
+            text "Loading ..."
+
+        Failure e ->
+            text (toString e)
+
+        Success data ->
+            case List.length data of
+                0 ->
+                    text "No results"
+
+                _ ->
+                    resultsTable model.selectedParams data
+
+
+resultsTable : List ( String, String ) -> List (Dict.Dict String JsonType) -> Html msg
+resultsTable fieldList results =
+    let
+        mkTh fld =
+            th [] [ text (prettyName fld) ]
+
+        fieldNames =
+            List.map Tuple.first fieldList
+
+        headerRow =
+            [ tr [] (List.map mkTh ("specimen__sample_name" :: fieldNames)) ]
+
+        resultRows =
+            List.map (mkResultRow fieldList) results
+    in
+    div []
+        [ text ("Found " ++ toString (List.length results))
+        , table [] (headerRow ++ resultRows)
+        ]
+
+
+mkResultRow : List ( String, String ) -> Dict.Dict String JsonType -> Html msg
+mkResultRow fieldList result =
+    let
+        getVal : String -> String
+        getVal fldName =
+            case Dict.get fldName result of
+                Just (StrType s) ->
+                    s
+
+                Just (IntType i) ->
+                    toString i
+
+                Just (FloatType f) ->
+                    toString f
+
+                Just (ValueType v) ->
+                    toString v
+
+                _ ->
+                    "NA"
+
+        mkTd : ( String, String ) -> Html msg
+        mkTd ( fldName, dataType ) =
+            let
+                align =
+                    if dataType == "number" then
+                        "right"
+                    else
+                        "left"
+            in
+            td [ style [ ( "text-align", align ) ] ] [ text (getVal fldName) ]
+    in
+    tr []
+        (List.map mkTd (( "specimen__sample_name", "string" ) :: fieldList))
+
+
+mkRestrictedParams :
+    Dict.Dict String String
+    -> WebData (List (Dict.Dict String JsonType))
+    -> Dict.Dict String String
+mkRestrictedParams curParams searchResults =
+    case searchResults of
+        Success data ->
+            let
+                keys =
+                    List.map Dict.keys data
+                        |> List.concat
+                        |> List.filter (\v -> v /= "_id")
+                        |> List.Extra.unique
+
+                types =
+                    List.filterMap (\k -> Dict.get k curParams) keys
+            in
+            Dict.fromList (List.map2 (,) keys types)
+
+        _ ->
+            Dict.empty
