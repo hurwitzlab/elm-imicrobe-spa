d9ecbf033728cc351152d6a0b17cb56c1546c4ba
Matt Bomhoff <mbomhoff@email.arizona.edu>
2017-08-07 13:11:01 -0700

    Restyle page

--- src/Page/Investigator.elm
+++ src/Page/Investigator.elm
@@ -1,7 +1,6 @@
 module Page.Investigator exposing (Model, Msg, init, update, view)
 
 import Data.Investigator
-import Data.Session as Session exposing (Session)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Http
@@ -77,6 +76,8 @@ view : Model -> Html Msg
 view model =
     div [ class "container" ]
         [ viewInvestigator model.investigator
+        , viewProjects model.investigator.projects
+        , viewSamples model.investigator.samples
         ]
 
 
@@ -103,48 +104,98 @@ viewInvestigator inv =
                     ]
                 ]
             ]
-        , div []
-            [ h2 [] [ text "Projects" ]
-            , viewProjects inv.projects
-            ]
-        , div []
-            [ h2 [] [ text "Samples" ]
-            , viewSamples inv.samples
-            ]
         ]
 
 
 viewProjects : List Data.Investigator.Project -> Html msg
 viewProjects projects =
-    case List.length projects of
-        0 ->
-            text "None"
+    let
+        numProjects =
+            List.length projects
 
-        _ ->
-            ul [] (List.map viewProject projects)
+        label =
+            case numProjects of
+                0 ->
+                    span [] []
+
+                _ ->
+                    span [ class "badge" ]
+                        [ text (toString numProjects)
+                        ]
+
+        body =
+            case numProjects of
+                0 ->
+                    text "None"
+
+                _ ->
+                    table [ class "table" ]
+                        (List.map viewProject projects)
+    in
+    div []
+        [ h2 []
+            [ text "Projects "
+            , label
+            ]
+        , body
+        ]
 
 
 viewProject : Data.Investigator.Project -> Html msg
 viewProject project =
-    li []
-        [ a [ Route.href (Route.Project project.project_id) ]
-            [ text project.project_name ]
+    tr []
+        [ td []
+            [ a [ Route.href (Route.Project project.project_id) ]
+                [ text project.project_name ]
+            ]
         ]
 
 
 viewSamples : List Data.Investigator.Sample -> Html msg
 viewSamples samples =
-    case List.length samples of
-        0 ->
-            text "None"
+    let
+        numSamples =
+            List.length samples
 
-        _ ->
-            ul [] (List.map viewSample samples)
+        label =
+            case numSamples of
+                0 ->
+                    span [] []
+
+                _ ->
+                    span [ class "badge" ]
+                        [ text (toString numSamples)
+                        ]
+        cols =
+            tr []
+                (List.map (\s -> th [] [ text s ]) [ "Name", "Type" ])
+
+        rows =
+            List.map viewSample samples
+
+        body =
+            case numSamples of
+                0 ->
+                    text "None"
+
+                _ ->
+                    table [] (cols :: rows)
+    in
+    div []
+        [ h2 []
+            [ text "Samples "
+            , label
+            ]
+        , body
+        ]
 
 
 viewSample : Data.Investigator.Sample -> Html msg
 viewSample sample =
-    li []
-        [ a [ Route.href (Route.Sample sample.sample_id) ]
-            [ text sample.sample_name ]
+    tr []
+        [ td []
+            [ a [ Route.href (Route.Sample sample.sample_id) ]
+                [ text sample.sample_name ]
+            ]
+        , td [] [ text sample.sample_type ]
         ]
