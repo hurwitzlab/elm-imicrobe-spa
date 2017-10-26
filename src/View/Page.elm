7dbd9a56274e1a9f57cfa31caf819ce69afe05fa
Matt Bomhoff <mbomhoff@email.arizona.edu>
2017-10-26 15:14:16 -0700

    Add notice banner

--- src/View/Page.elm
+++ src/View/Page.elm
@@ -108,68 +108,70 @@ viewHeader page isLoading session =
                     [ span [ class "icon-button glyphicon glyphicon-question-sign" ] [] ]
                 ]
     in
-    nav [ class "navbar navbar-default navbar-static-top", style [("padding-top", "10px")] ]
-        [ div [ class "container" ]
-            [ div [ class "navbar-header" ]
-                [ a [ class "navbar-brand", Route.href Route.Home ]
-                    [ img [ src "/img/nav-header.png" ] [] ]
-                ]
-            , div [ class "navbar-collapse collapse" ]
-                [ ul [ class "nav navbar-nav" ]
-                    [ li [ class "dropdown" ]
-                        [ a [ class "dropdown-toggle", attribute "data-toggle" "dropdown", attribute "role" "button", attribute "aria-expanded" "false" ]
-                            [ text "Search"
-                            , span [ class "caret" ] []
-                            ]
-                        , ul
-                            [ class "dropdown-menu", style [ ( "role", "menu" ) ] ]
-                            [ li [] [ a [ Route.href Route.Search ] [ text "General Search" ] ]
-                            , li [] [ a [ Route.href Route.MetaSearch ] [ text "Sample Search" ] ]
-                            ]
-                        ]
-                    , li [ class "dropdown" ]
-                        [ a [ class "dropdown-toggle", attribute "data-toggle" "dropdown", attribute "role" "button", attribute "aria-expanded" "false" ]
-                            [ text "Browse"
-                            , span [ class "caret" ] []
-                            ]
-                        , ul
-                            [ class "dropdown-menu", style [ ( "role", "menu" ) ] ]
-                            [ li [] [ a [ Route.href Route.Investigators ] [ text "Investigators" ] ]
-                            , li [] [ a [ Route.href Route.Projects ] [ text "Projects" ] ]
---                            , li [] [ a [ Route.href Route.ProjectGroups ] [ text "Project Groups" ] ]
---                            , li [] [ a [ Route.href Route.Domains ] [ text "Domains" ] ]
---                            , li [] [ a [ Route.href Route.Assemblies ] [ text "Assemblies" ] ]
---                            , li [] [ a [ Route.href Route.CombinedAssemblies ] [ text "CombinedAssemblies" ] ]
-                            , li [] [ a [ Route.href Route.Samples ] [ text "Samples" ] ]
-                            , li [] [ a [ Route.href Route.Publications ] [ text "Publications" ] ]
-                            , li [] [ a [ Route.href Route.Pubchase ] [ text "Recommended Readings" ] ]
-                            ]
-                        ]
-                    , li [ class "dropdown" ]
-                        [ a [ class "dropdown-toggle", attribute "data-toggle" "dropdown", attribute "role" "button", attribute "aria-expanded" "false" ]
-                            [ text "Tools"
-                            , span [ class "caret" ] []
-                            ]
-                        , ul
-                            [ class "dropdown-menu", style [ ( "role", "menu" ) ] ]
-                            [ li [] [ a [ Route.href Route.Apps ] [ text "Apps" ] ]
-                            , li [] [ a [ Route.href Route.Jobs ] [ text "Jobs" ] ]
-                            , li [] [ a [ Route.href Route.Cart ] [ text ("Cart " ++ cartLabel) ] ]
-                            ]
-                        ]
-                    , li []
-                        [ a [ href "ftp://ftp.imicrobe.us" ]
-                            [ text "Download" ]
-                        ]
-                    , loginMenuItem
+    div []
+        [ div [ class "center border-bottom", style [("background-color", "#ffb84d"), ("color", "white")] ] [ text "Note: This is a new beta version and still currently under development! Please ", a [ href "#/contact" ] [ text "let us know" ], text " of any issues or suggestions." ]
+        , nav [ class "navbar navbar-default navbar-static-top", style [("padding-top", "10px")] ]
+            [ div [ class "container" ]
+                [ div [ class "navbar-header" ]
+                    [ a [ class "navbar-brand", Route.href Route.Home ]
+                        [ img [ src "/img/nav-header.png" ] [] ]
                     ]
-                , helpButton
-                , cartButton
-                , searchBar
+                , div [ class "navbar-collapse collapse" ]
+                    [ ul [ class "nav navbar-nav" ]
+                        [ li [ class "dropdown" ]
+                            [ a [ class "dropdown-toggle", attribute "data-toggle" "dropdown", attribute "role" "button", attribute "aria-expanded" "false" ]
+                                [ text "Search"
+                                , span [ class "caret" ] []
+                                ]
+                            , ul
+                                [ class "dropdown-menu", style [ ( "role", "menu" ) ] ]
+                                [ li [] [ a [ Route.href Route.Search ] [ text "General Search" ] ]
+                                , li [] [ a [ Route.href Route.MetaSearch ] [ text "Sample Search" ] ]
+                                ]
+                            ]
+                        , li [ class "dropdown" ]
+                            [ a [ class "dropdown-toggle", attribute "data-toggle" "dropdown", attribute "role" "button", attribute "aria-expanded" "false" ]
+                                [ text "Browse"
+                                , span [ class "caret" ] []
+                                ]
+                            , ul
+                                [ class "dropdown-menu", style [ ( "role", "menu" ) ] ]
+                                [ li [] [ a [ Route.href Route.Investigators ] [ text "Investigators" ] ]
+                                , li [] [ a [ Route.href Route.Projects ] [ text "Projects" ] ]
+--                                , li [] [ a [ Route.href Route.ProjectGroups ] [ text "Project Groups" ] ]
+--                                , li [] [ a [ Route.href Route.Domains ] [ text "Domains" ] ]
+--                                , li [] [ a [ Route.href Route.Assemblies ] [ text "Assemblies" ] ]
+--                                , li [] [ a [ Route.href Route.CombinedAssemblies ] [ text "CombinedAssemblies" ] ]
+                                , li [] [ a [ Route.href Route.Samples ] [ text "Samples" ] ]
+                                , li [] [ a [ Route.href Route.Publications ] [ text "Publications" ] ]
+                                , li [] [ a [ Route.href Route.Pubchase ] [ text "Recommended Readings" ] ]
+                                ]
+                            ]
+                        , li [ class "dropdown" ]
+                            [ a [ class "dropdown-toggle", attribute "data-toggle" "dropdown", attribute "role" "button", attribute "aria-expanded" "false" ]
+                                [ text "Tools"
+                                , span [ class "caret" ] []
+                                ]
+                            , ul
+                                [ class "dropdown-menu", style [ ( "role", "menu" ) ] ]
+                                [ li [] [ a [ Route.href Route.Apps ] [ text "Apps" ] ]
+                                , li [] [ a [ Route.href Route.Jobs ] [ text "Jobs" ] ]
+                                , li [] [ a [ Route.href Route.Cart ] [ text ("Cart " ++ cartLabel) ] ]
+                                ]
+                            ]
+                        , li []
+                            [ a [ href "ftp://ftp.imicrobe.us" ]
+                                [ text "Download" ]
+                            ]
+                        , loginMenuItem
+                        ]
+                    , helpButton
+                    , cartButton
+                    , searchBar
+                    ]
                 ]
             ]
         ]
-
 
 viewFooter : Html msg
 viewFooter =
