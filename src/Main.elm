d3dd6618713fd9f89ce0f891f93b029f12f3d17f
Matt Bomhoff <mbomhoff@email.arizona.edu>
2017-11-30 10:56:16 -0700

    Remove banner with beta message

--- src/Main.elm
+++ src/Main.elm
@@ -1330,8 +1330,8 @@ viewHeader page isLoading session =
                 ]
     in
     div []
-        [ div [ class "center border-bottom", style [("background-color", "#ffb84d"), ("color", "white")] ] [ text "Note: This is a new beta version and still currently under development.  Please ", a [ Html.Attributes.href "#/contact" ] [ text "let us know" ], text " of any issues or suggestions!" ]
-        , nav [ class "navbar navbar-default navbar-static-top", style [("padding-top", "10px")] ]
+        [ --div [ class "center border-bottom", style [("background-color", "#ffb84d"), ("color", "white")] ] [ text "Note: This is a new beta version and still currently under development.  Please ", a [ Html.Attributes.href "#/contact" ] [ text "let us know" ], text " of any issues or suggestions!" ]
+          nav [ class "navbar navbar-default navbar-static-top", style [("padding-top", "10px")] ]
             [ div [ class "container" ]
                 [ div [ class "navbar-header" ]
                     [ a [ class "navbar-brand", Html.Attributes.href "/" ] --Route.href Route.Home ] -- fix home page not rendering in some cases
