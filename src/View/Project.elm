e49e2e6de0746aade2e1b2befd8254486285d1b0
Matt Bomhoff <mbomhoff@email.arizona.edu>
2018-03-28 16:10:12 -0700

    Add actions

--- src/View/Project.elm
+++ src/View/Project.elm
@@ -1,45 +1,86 @@
-module View.Project exposing (viewInfo)
+module View.Project exposing (viewInfo, viewActions)
 
-import Html exposing (Html, div, a, table, th, tr, td, text)
+import Html exposing (Html, div, a, table, th, tr, td, text, button, span)
 import Html.Attributes exposing (class)
-import Data.Project exposing (Investigator, Publication)
+import Html.Events exposing (onClick)
+import Data.Project exposing (Investigator, Publication, Sample)
 import Route
 
 
-viewInfo : { a | project_name : String, project_code : String, project_type : String, url : String, investigators : List Investigator, publications : List Publication } -> Html msg
-viewInfo { project_name, project_code, project_type, url, investigators, publications } =
-    div [ class "col-md-4" ]
-        [ table [ class "info-table" ]
-            [ tr []
-                [ th [] [ text "Name " ]
-                , td [] [ text project_name ]
-                ]
-            , tr []
-                [ th [] [ text "Code " ]
-                , td [] [ text project_code ]
-                ]
-            , tr []
-                [ th [] [ text "Type " ]
-                , td [] [ text project_type ]
-                ]
-            , tr []
-                [ th [] [ text "URL " ]
-                , td [] [ text url ]
-                ]
---            , tr []
---                [ th [] [ text "PI " ]
---                , td [] [ text project.pi ]
---                ]
-            , tr []
-                [ th [] [ text "Investigators " ]
-                , td [] (viewInvestigators investigators)
-                ]
-            , tr []
-                [ th [] [ text "Publications " ]
-                , td [] [ viewPublications publications ]
+viewInfo
+  : { a
+    | project_name : String
+    , project_code : String
+    , project_type : String
+    , url : String
+    , investigators : List Investigator
+    , publications : List Publication
+    , sample_count : Int
+    }
+  -> Html msg
+viewInfo { project_name, project_code, project_type, url, investigators, publications, sample_count } =
+    let
+        numSamples =
+            if sample_count == 0 then
+                "None"
+            else
+                toString sample_count
+    in
+    div [ class "row" ]
+        [ div [ class "col-md-4" ]
+            [ table [ class "info-table" ]
+                [ tr []
+                    [ th [] [ text "Name " ]
+                    , td [] [ text project_name ]
+                    ]
+                , tr []
+                    [ th [] [ text "Code " ]
+                    , td [] [ text project_code ]
+                    ]
+                , tr []
+                    [ th [] [ text "Type " ]
+                    , td [] [ text project_type ]
+                    ]
+                , tr []
+                    [ th [] [ text "URL " ]
+                    , td [] [ text url ]
+                    ]
+--              , tr []
+--                  [ th [] [ text "PI " ]
+--                  , td [] [ text project.pi ]
+--                  ]
+                , tr []
+                    [ th [] [ text "Investigators " ]
+                    , td [] (viewInvestigators investigators)
+                    ]
+                , tr []
+                    [ th [] [ text "Publications " ]
+                    , td [] [ viewPublications publications ]
+                    ]
+                , tr []
+                    [ th [] [ text "Samples " ]
+                    , td [] [ text numSamples ]
+                    ]
                 ]
             ]
         ]
+
+
+viewActions : msg -> Html msg
+viewActions deleteMsg =
+        div [ class "row" ]
+            [ div [ class "col-md-4" ]
+                [ table [ class "info-table" ]
+                    [ tr []
+                        [ td []
+                            [ button [ class "btn btn-link btn-xs", onClick deleteMsg ]
+                                [ span [ class "glyphicon glyphicon-trash" ] [], text " Delete"
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
 
 
 viewInvestigators : List Investigator -> List (Html msg)
--- src/View/Sample.elm
+++ src/View/Sample.elm
@@ -1,32 +1,59 @@
-module View.Sample exposing (viewInfo)
+module View.Sample exposing (viewInfo, viewActions)
 
-import Html exposing (Html, div, table, th, tr, td, text)
+import Html exposing (Html, div, table, th, tr, td, text, button, span)
 import Html.Attributes exposing (class)
+import Html.Events exposing (onClick)
 
 
 viewInfo : { a | sample_name : String, sample_acc : String, sample_type : String } -> Html msg
 viewInfo { sample_name, sample_acc, sample_type } =
-    div [ class "col-md-4" ]
-        [ table [ class "info-table" ]
-            [ tr []
-                [ th [] [ text "Name " ]
-                , td [] [ text sample_name ]
+    div [ class "row" ]
+        [ div [ class "col-md-4" ]
+            [ table [ class "info-table" ]
+                [ tr []
+                    [ th [] [ text "Name " ]
+                    , td [] [ text sample_name ]
+                    ]
+                , tr []
+                    [ th [] [ text "Code " ]
+                    , td [] [ text sample_acc ]
+                    ]
+                , tr []
+                    [ th [] [ text "Type " ]
+                    , td [] [ text sample_type ]
+                    ]
+--               , tr []
+--                   [ th [] [ text "Project " ]
+--                    , td [] [ text sample.project.project_name ]
+--                    ]
+--                , tr []
+--                    [ th [] [ text "Files " ]
+--                   , td [] [ text "" ]
+--                    ]
                 ]
-            , tr []
-                [ th [] [ text "Code " ]
-                , td [] [ text sample_acc ]
+            ]
+        ]
+
+
+viewActions : msg -> msg -> Html msg
+viewActions viewMsg deleteMsg =
+    div [ class "row" ]
+        [ div [ class "col-md-4" ]
+            [ table [ class "info-table" ]
+                [ tr []
+                    [ td []
+                        [ button [ class "btn btn-link btn-xs", onClick viewMsg ]
+                            [ span [ class "glyphicon glyphicon-eye" ] [], text " View"
+                            ]
+                        ]
+                    ]
+                , tr []
+                    [ td []
+                        [ button [ class "btn btn-link btn-xs", onClick deleteMsg ]
+                            [ span [ class "glyphicon glyphicon-trash" ] [], text " Delete"
+                            ]
+                        ]
+                    ]
                 ]
-            , tr []
-                [ th [] [ text "Type " ]
-                , td [] [ text sample_type ]
-                ]
---            , tr []
---                [ th [] [ text "Project " ]
---                , td [] [ text sample.project.project_name ]
---                ]
---            , tr []
---                [ th [] [ text "Files " ]
---                , td [] [ text "" ]
---                ]
             ]
         ]
