5d50960c918172f23371b9115fcf6f95b9451448
Matt Bomhoff <mbomhoff@email.arizona.edu>
2018-07-25 12:08:29 -0700

    Formatting

--- webpack.config.js
+++ webpack.config.js
@@ -12,42 +12,44 @@ module.exports = {
     module: {
       noParse: /\.elm$/,
       rules: [
-      {
-        test: /\.(css|scss)$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ]
-      },
-      {
-        test:    /\.html$/,
-        exclude: /node_modules/,
-        loader:  'file-loader?name=[name].[ext]',
-      },
-      {
-        test:    /\.elm$/,
-        exclude: [/elm-stuff/, /node_modules/],
-        loader:  'elm-webpack-loader?verbose=true&warn=true',
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader',
-      },
-      {
-        test: /\.(jpe?g|png|gif|ico)$/,
-        loader: 'file-loader?name=./img/[name].[ext]',
-      },
-      { test: /vendor\/.+\.(jsx|js)$/,
-        loader: 'imports-loader?jQuery=jquery,$=jquery,this=>window'
-      },
-      { test: /bootstrap\/js\//,
-        loader: 'imports-loader?jQuery=jquery'
-      },
-    ]
+          {
+            test: /\.(css|scss)$/,
+            use: [
+              'style-loader',
+              'css-loader',
+            ]
+          },
+          {
+            test:    /\.html$/,
+            exclude: /node_modules/,
+            loader:  'file-loader?name=[name].[ext]',
+          },
+          {
+            test:    /\.elm$/,
+            exclude: [/elm-stuff/, /node_modules/],
+            loader:  'elm-webpack-loader?verbose=true&warn=true',
+          },
+          {
+            test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+          },
+          {
+            test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            loader: 'file-loader',
+          },
+          {
+            test: /\.(jpe?g|png|gif|ico)$/,
+            loader: 'file-loader?name=./img/[name].[ext]',
+          },
+          {
+            test: /vendor\/.+\.(jsx|js)$/,
+            loader: 'imports-loader?jQuery=jquery,$=jquery,this=>window'
+          },
+          {
+            test: /bootstrap\/js\//,
+            loader: 'imports-loader?jQuery=jquery'
+          },
+      ]
   },
   plugins: [
     new webpack.ProvidePlugin({
